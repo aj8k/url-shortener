e9374549c799847e164ff18c8e2b7d0992b15d73
Anton Joy <anton@antonjoy.com>
2026-01-26 01:08:33 +0200

    Added full name shortlinks

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,18 +1,37 @@
 export const redirects: Readonly<Record<string, string>> = {
 	// Social Links
 	li: 'https://www.linkedin.com/in/antonjoy/',
+	linkedin: 'https://www.linkedin.com/in/antonjoy/',
+
 	ph: 'https://www.producthunt.com/@antonjoy',
+	producthunt: 'https://www.producthunt.com/@antonjoy',
+
 	dt: 'https://dev.to/antonjoy',
+	devto: 'https://dev.to/antonjoy',
+
 	bs: 'https://bsky.app/profile/antonjoy.com',
+	bsky: 'https://bsky.app/profile/antonjoy.com',
+	bluesky: 'https://bsky.app/profile/antonjoy.com',
+
 	x: 'https://x.com/antonjoy_dev',
+
 	yt: 'https://www.youtube.com/@antonjoy_dev',
+	youtube: 'https://www.youtube.com/@antonjoy_dev',
+
 	ih: 'https://www.indiehackers.com/antonjoy',
+	indiehackers: 'https://www.indiehackers.com/antonjoy',
+
 	hn: 'https://news.ycombinator.com/user?id=antonjoy',
+	hackernews: 'https://news.ycombinator.com/user?id=antonjoy',
 
 	// Infrastructure Links
 	gh: 'https://github.com/aj8k',
+	github: 'https://github.com/aj8k',
+
 	npm: 'https://www.npmjs.com/~aj8k',
+
 	dh: 'https://hub.docker.com/u/antonjoy',
+	dockerhub: 'https://hub.docker.com/u/antonjoy',
 
 	// Me
 	me: 'https://antonjoy.com/',
